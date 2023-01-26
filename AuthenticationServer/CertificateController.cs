0587aadbb29b01939972461d82bf018aa6b1b0d3
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-26 08:40:49 +0100

    Add project files.

---
+++ AuthenticationServer/CertificateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace phd_thesis.Controllers
+{
+    public class CertificateController
+    {
+        [Route("/certificate/download")]
+        public string Generate(string username)
+        {
+            //This code creates a 2048-bit key
+            using (var rsa = RSA.Create(2048))
+            {
+                return Convert.ToBase64String(rsa.ExportRSAPublicKey());
+            }
+        }
+    }
+}
---
+++ phd-thesis/Pages/GenerateCertificate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace phd_thesis
+{
+    public class GenerateCertificateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
