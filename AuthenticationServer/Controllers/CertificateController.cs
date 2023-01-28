045f53a3f67fba6bb7503e46a67da52049cd5ada
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-28 14:47:54 +0100

    Move controllers

--- AuthenticationServer/Controllers/CertificateController.cs
+++ AuthenticationServer/Controllers/CertificateController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 
-namespace phd_thesis.Controllers
+namespace AuthenticationServer.Controllers
 {
-    public class VerifyController
+    public class CertificateController
     {
-        [Route("/verify")]
-        public object Verify(string payload, string @return)
+        [Route("/certificate/generate")]
+        public object Generate()
         {
             //This code creates a 2048-bit key
             using (var rsa = RSA.Create(2048))
--- AuthenticationServer/Controllers/VerifyController.cs
+++ AuthenticationServer/Controllers/VerifyController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 
-namespace phd_thesis.Controllers
+namespace AuthenticationServer.Controllers
 {
-    public class CertificateController
+    public class VerifyController
     {
-        [Route("/certificate/generate")]
-        public object Generate()
+        [Route("/verify")]
+        public object Verify(string payload, string @return)
         {
             //This code creates a 2048-bit key
             using (var rsa = RSA.Create(2048))
